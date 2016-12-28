8575e26c70bd229a9d08a2be7cca3ef762789a95
Pavel <piv@sitecore.net>
2016-12-28 13:49:07 +0200

    Fix issue #392562: Removing broken links from the HtmlField causes removing all links

---
+++ src/Sitecore.Support.392562/Data/Fields/HtmlField.cs
@@ -0,0 +1,86 @@
+﻿using HtmlAgilityPack;
+using Sitecore.Data;
+using Sitecore.Data.Fields;
+using Sitecore.Diagnostics;
+using Sitecore.Layouts;
+using Sitecore.Links;
+using Sitecore.Web;
+
+namespace Sitecore.Support.Data.Fields
+{
+  public class HtmlField : Sitecore.Data.Fields.HtmlField
+  {
+    public HtmlField(Field innerField) : base(innerField)
+    {
+    }
+
+    private ID GetLinkedID(string href)
+    {
+      DynamicLink link;
+      Assert.ArgumentNotNull(href, "href");
+      try
+      {
+        link = DynamicLink.Parse(href);
+      }
+      catch (InvalidLinkFormatException)
+      {
+        return null;
+      }
+      return link.ItemId;
+    }
+
+    public override void RemoveLink(ItemLink itemLink)
+    {
+      Assert.ArgumentNotNull(itemLink, "itemLink");
+      if (!InnerField.HasValue) return;
+      var document = new HtmlDocument();
+      document.LoadHtml(Value);
+
+      if (!(RemoveTextLinks(itemLink, document) | RemoveMediaLinks(itemLink, document))) return;
+      RuntimeHtml.FixBullets(document);
+      RuntimeHtml.FixSelectOptions(document);
+      Value = document.DocumentNode.OuterHtml;
+    }
+
+    private bool RemoveMediaLinks(ItemLink itemLink, HtmlDocument document)
+    {
+      Assert.ArgumentNotNull(itemLink, "itemLink");
+      Assert.ArgumentNotNull(document, "document");
+      var nodes = document.DocumentNode.SelectNodes("//img");
+      if (nodes == null) return false;
+      var flag = false;
+
+      foreach (var node in nodes)
+      {
+        var attributeValue = node.GetAttributeValue("src", string.Empty);
+        if (string.IsNullOrEmpty(attributeValue)) continue;
+        var linkedID = GetLinkedID(attributeValue);
+        if ((linkedID == (ID) null) || (linkedID != itemLink.TargetItemID)) continue;
+        node.ParentNode.RemoveChild(node, true);
+        flag = true;
+      }
+      return flag;
+    }
+
+    private bool RemoveTextLinks(ItemLink itemLink, HtmlDocument document)
+    {
+      Assert.ArgumentNotNull(itemLink, "itemLink");
+      Assert.ArgumentNotNull(document, "document");
+      var nodes = document.DocumentNode.SelectNodes("//a[@href]");
+      if (nodes == null) return false;
+      var flag = false;
+
+      foreach (var node in nodes)
+      {
+        var attributeValue = node.GetAttributeValue("href", string.Empty);
+        if (string.IsNullOrEmpty(attributeValue)) continue;
+        var linkedID = GetLinkedID(attributeValue);
+
+        if ((linkedID == (ID) null) || linkedID != itemLink.TargetItemID) continue;
+        node.ParentNode.RemoveChild(node, true);
+        flag = true;
+      }
+      return flag;
+    }
+  }
+}
---
+++ src/Sitecore.Support.392562/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.392562")]
+[assembly: AssemblyProduct("Sitecore.Support.392562")]
+[assembly: ComVisible(false)]
